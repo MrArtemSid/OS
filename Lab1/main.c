0495adc0f9d5a1fe91ad3fc28e221286870677d1
MrArtemSid <artem.sid.key@gmail.com>
2024-03-09 16:37:04 +0400

    OS: Lab1: Add comments

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
+// Команда 'cd': изменяет текущую рабочую директорию на указанную
 int fn_cd(char **args) {
-    if (args[1] == NULL) {
+    if (args[1] == NULL) { // при отсутствии аргумента вывести текущий путь
         char path[256];
         getcwd(path,256);
         printf("%s\n", path);
         return 0;
     }
 
-    if (chdir(args[1])) {
+    if (chdir(args[1])) { // попытка сменить директорию
         perror("cd");
         return 0;
     }
 
     return 0;
 }
+
+// Команда 'clr': очищает экран
 int fn_clr(char **args) {
     printf("\033c");
     return 0;
 }
 
+// Команда 'dir': выводит список всех файлов в указанной директории
 int fn_dir(char **args) {
     if (args[1] == NULL) {
         printf("Usage: dir <directory>\n");
@@ -35,7 +39,7 @@ int fn_dir(char **args) {
         return 0;
     }
 
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL) { // вывод списка файлов и папок
         printf("%s\n", entry->d_name);
     }
 
@@ -43,6 +47,7 @@ int fn_dir(char **args) {
     return 0;
 }
 
+// Команда 'environ': отображает все переменные среды
 int fn_environ(char **args) {
     extern char **environ;
 
@@ -54,6 +59,7 @@ int fn_environ(char **args) {
     return 0;
 }
 
+// Команда 'echo': отображает указанный текст
 int fn_echo(char **args) {
     if (args[1] == NULL) {
         printf("Usage: echo <something>\n");
@@ -63,6 +69,7 @@ int fn_echo(char **args) {
     return 0;
 }
 
+// Команда 'help': выводит список доступных команд
 int fn_help(char **args) {
     printf("Available commands:\n");
     for (int i = 0; i < fn_cnt(); i++) {
@@ -72,16 +79,19 @@ int fn_help(char **args) {
     return 0;
 }
 
+// Команда 'pause': приостанавливает выполнение оболочки до нажатия клавиши
 int fn_pause(char **args) {
     printf("Press any key...\n");
     getchar();
     return 0;
 }
 
+// Команда 'quit': завершает выполнение оболочки
 int fn_quit(char **args) {
     return -1;
 }
 
+// Список команд
 char * fn_list[] = {
     "cd",
     "clr",
@@ -93,6 +103,7 @@ char * fn_list[] = {
     "quit"
 };
 
+// Описание команд
 char * fn_list_help[] = {
     "cd <directory> - change directory to <directory>. If argument <directory> isn't present, then output current one.",
     "clr - clears the screen",
@@ -104,6 +115,7 @@ char * fn_list_help[] = {
     "quit - exits from shell"
 };
 
+// Массив указателей на функции для каждой команды
 int (*fns[]) (char **) = {
     &fn_cd,
     &fn_clr,
@@ -115,20 +127,23 @@ int (*fns[]) (char **) = {
     &fn_quit,
 };
 
+// Возвращает количество команд
 int fn_cnt () {
     return sizeof (fn_list) / sizeof (char *);
 }
 
+// Основная функция оболочки
 int main(int argc, char **argv) {
+    // Переключение на ввод с файла, если предоставлен аргумент или же вывод ошибки если файла не существует
     if (argc == 2 && freopen(argv[1], "r", stdin) == NULL) {
         printf("Error: input file\n");
         exit(1);
     }
 
-    char * inp_ptr;
-    size_t n;
-    int status = 0;
-    char path[256];
+    char * inp_ptr; // указатель на строку
+    size_t n; // размер строки
+    int status = 0; // статус выполнения, если не 0, то программа завершается
+    char path[256]; // массив для сохранения пути
 
     getcwd(path, 256);
     setenv("shell", strcat(path, "/myshell"), 1);
@@ -138,34 +153,33 @@ int main(int argc, char **argv) {
         printf("(Shell) [%s] $ ", path);
 
         inp_ptr = NULL;
-        getline(&inp_ptr, &n, stdin);
+        getline(&inp_ptr, &n, stdin); // ввод пользователя
         char **args = malloc(2 * sizeof(char *));
-        args[0] = strtok(inp_ptr, " \n");
-        args[1] = strtok(NULL, " \n");
+        args[0] = strtok(inp_ptr, " \n"); // сохранение команды
+        args[1] = strtok(NULL, " \n"); // сохранение аргумента команды
 
         int i;
-        for (i = 0; i < fn_cnt(); ++i) {
+        for (i = 0; i < fn_cnt(); ++i) { // цикл для поиска и запуска введеных команд
             if (args[0] != NULL && strcmp(args[0], fn_list[i]) == 0) {
                 status = (*fns[i])(args);
                 break;
             }
         }
 
-        if (i == fn_cnt()) {
-            pid_t pid = fork();
+        if (i == fn_cnt()) { // если команды нет в списке существующих, то запуск программ
+            pid_t pid = fork(); // клонирование процесса
 
             if (pid == 0) {
-                // Child process
+                // Child процесс
                 execvp(args[0], args);
                 perror("execvp");
                 exit(EXIT_FAILURE);
             } else if (pid < 0) {
                 perror("fork");
             } else {
-                wait(NULL); // Wait for child process to finish
+                wait(NULL); // Ожидание завершения child процесса
             }
         }
-
 
         n = 0;
         free(inp_ptr);
