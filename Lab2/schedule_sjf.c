3a3ff9a48e0486e71d25f724eb221077ac46d45e
MrArtemSid <artem.sid.key@gmail.com>
2024-03-16 23:15:33 +0400

    OS: Lab2: Implement SJF scheduler

---
+++ Lab2/schedule_sjf.c
@@ -0,0 +1,35 @@
+#include "list.h"
+#include "cpu.h"
+#include "schedulers.h"
+#include "priority_queue.h"
+
+struct PriorityQueue pq = {{0}, 0};
+
+Task *pickNextTask() {
+    Task newTask = extractMin(&pq);
+    if (newTask.burst == -1)
+        return NULL;
+
+    Task *ptrNewTask = malloc(sizeof(Task));
+
+    *ptrNewTask = newTask;
+
+    return ptrNewTask;
+}
+
+void add(char *name, int priority, int burst) {
+    Task newTask;
+    newTask.name = name;
+    newTask.priority = priority;
+    newTask.burst = burst;
+
+    insert_pq(&pq, newTask);
+}
+
+void schedule() {
+    Task *currTask;
+    while ((currTask = pickNextTask())) {
+        run(currTask, currTask->burst);
+        free(currTask);
+    }
+}
