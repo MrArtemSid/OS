278cffc21b830fec621f326846261a5e24079de4
MrArtemSid <artem.sid.key@gmail.com>
2024-03-17 02:38:14 +0400

    OS: Lab2: Implement RR scheduler

---
+++ Lab2/schedule_rr.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task.h"
+#include "list.h"
+#include "cpu.h"
+#include "schedulers.h"
+
+struct node *tasks = NULL;
+struct node *prev = NULL;
+
+int min(int a, int b) {
+    return a <= b ? a : b;
+}
+
+Task *pickNextTask() {
+    if (!prev)
+        return NULL;
+
+    Task *nextTask = prev->task;
+    prev = prev->next;
+
+    return nextTask;
+}
+
+void add(char *name, int priority, int burst) {
+    Task *newTask = malloc(sizeof(Task));
+    newTask->name = name;
+    newTask->priority = priority;
+    newTask->burst = burst;
+    insert(&tasks, newTask);
+}
+
+void schedule_helper_cycle() {
+    Task *currTask;
+    while ((currTask = pickNextTask())) {
+        int delta = min(QUANTUM, currTask->burst);
+        run(currTask, delta);
+        currTask->burst -= delta;
+        if (!currTask->burst) {
+            delete(&tasks, currTask);
+            return;
+        }
+    }
+}
+
+void schedule() {
+    while (tasks) {
+        schedule_helper_cycle();
+        prev = tasks;
+    }
+}
+
+
+/*
+void schedule_helper(Task *currTask) {
+    if (!currTask) {
+        return;
+    }
+
+    Task *nextTask = pickNextTask();
+    schedule_helper(nextTask);
+    int delta = min(QUANTUM, currTask->burst);
+    run(currTask, delta);
+    currTask->burst -= delta;
+    if (!currTask->burst) {
+        delete(&tasks, currTask);
+        return;
+    }
+}
+
+void schedule_rec() {
+    while (tasks) {
+        Task *currTask = pickNextTask();
+        schedule_helper(currTask);
+        prev = tasks;
+    }
+}
+*/
