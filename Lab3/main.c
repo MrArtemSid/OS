306d116a5363a5e99a9bad38399d3a570dc4c3b6
MrArtemSid <artem.sid.key@gmail.com>
2024-03-25 23:06:44 +0400

    OS: Lab3: Implement TLB, Page tables and demand paging

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,18 +1,75 @@
 #include <stdio.h>
+#include <string.h>
 
-const int PAGE_SIZE = 1<<8;
-const int FRAME_SIZE = 1<<8;
-const int offset_mask = (1<<8) - 1;
+#define PAGE_CNT 1 << 8
+#define PAGE_SIZE 1 << 8
+#define TLB_CNT 16
+#define FRAME_SIZE 1 << 8
 
-char tlb [16];
-//char pages[PAGE_SIZE];
+const int offset_mask = (1 << 8) - 1;
+
 FILE *backing_store;
 
 struct page {
     int offset;
     int n;
-    int value;
+    int frame_n;
 };
+
+struct page tlb [TLB_CNT];
+struct page pages [PAGE_CNT];
+char ram[PAGE_CNT][FRAME_SIZE];
+char buffer[PAGE_SIZE];
+
+char tlb_cnt = 0;
+int frame_cnt = 0;
+int page_cnt = 0;
+
+int read_from_file(struct page *curr_page) {
+    if (frame_cnt >= PAGE_CNT || page_cnt >= PAGE_CNT) {
+        return -1;
+    }
+
+    fseek(backing_store, curr_page->n * FRAME_SIZE, SEEK_SET);
+    fread(buffer, sizeof(signed char), FRAME_SIZE, backing_store);
+    memcpy(ram[frame_cnt], buffer, FRAME_SIZE);
+
+    pages[page_cnt].n = curr_page->n;
+    pages[page_cnt].frame_n = frame_cnt;
+    page_cnt++;
+
+    return frame_cnt++;
+}
+
+void insert_tlb(struct page *curr_page) {
+    if (tlb_cnt == 16) {
+        memmove(&tlb[1], &tlb[2], sizeof(struct page) * 15);
+        tlb_cnt--;
+    }
+    tlb[tlb_cnt++] = *curr_page;
+}
+
+int get_frame(struct page *curr_page) {
+    for (int i = tlb_cnt; i >= 0; --i) {
+        if (tlb[i].n == curr_page->n) {
+            curr_page->frame_n = tlb[i].frame_n;
+            goto end;
+        }
+    }
+
+    for (int i = 0; i < page_cnt; ++i) {
+        if (pages[i].n == curr_page->n) {
+            curr_page->frame_n = pages[i].frame_n;
+            goto end;
+        }
+    }
+
+    curr_page->frame_n = read_from_file(curr_page);
+
+end:
+    insert_tlb(curr_page);
+    return 0;
+}
 
 struct page get_page() {
     int tmp;
@@ -28,11 +85,17 @@ int main(int argc, char *argv[]) {
         return -1;
     if (!freopen("check.txt", "w", stdout))
         return -1;
+
+    backing_store = fopen("../BACKING_STORE.bin", "rb");
+
     for (int i = 0; i < 1000; ++i) {
         struct page curr_page = get_page();
-        printf("%d %d\n", curr_page.offset, curr_page.n);
+        get_frame(&curr_page);
+        printf("Virtual address: %d Physical address: %d Value: %d\n",
+               (curr_page.n << 8) | curr_page.offset,
+               (curr_page.frame_n << 8) | curr_page.offset,
+               ram[curr_page.frame_n][curr_page.offset]);
     }
-    backing_store = fopen("BACKING_STORE.bin", "rb");
 
     return 0;
 }
